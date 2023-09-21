8df2e21eef7795b0aa542c38f91104413af3df8c
Sophia Palomo <122858533+TinyTiim@users.noreply.github.com>
2023-09-21 13:53:14 -0700

    Updated script.js
    Fixing functionaility with the function update time

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,12 @@
 //This wraps all my code using jquery.
 $(document).ready(function () {
   $("button").on("click", function () {
-    var textToSave = $("textarea").val();
+    var textToSave = $(this).siblings("textarea").val();
+    var blockId = $(this).parent().attr("id");
+    localStorage.setItem(blockId, textToSave);
     //everytime you click the button it will relay what time you save and what you have scheduled for that day.
     console.log(
-      "You saved," +
+      "You saved, " +
         textToSave +
         " at this time, " +
         dayjsObject.format("h:mm.ss")
@@ -22,30 +24,19 @@ $(document).ready(function () {
   });
 
   //These are to track the time for the function to below so they can correlate to the correct hour.
-  let schedule8am = $("#hour-8");
-  let schedule9am = $("#hour-9");
-  let schedule10am = $("#hour-10");
-  let schedule11am = $("#hour-11");
-  let schedule12am = $("#hour-12");
-  let schedule1pm = $("#hour-1");
-  let schedule2pm = $("#hour-2");
-  let schedule3pm = $("#hour-3");
-  let schedule4pm = $("#hour-4");
-  let schedule5pm = $("#hour-5");
-  let schedule6pm = $("#hour-6");
 
   let scheduleElArray = [
-    schedule8am,
-    schedule9am,
-    schedule10am,
-    schedule11am,
-    schedule12am,
-    schedule1pm,
-    schedule2pm,
-    schedule3pm,
-    schedule4pm,
-    schedule5pm,
-    schedule6pm,
+    $("#hour-8"),
+    $("#hour-9"),
+    $("#hour-10"),
+    $("#hour-11"),
+    $("#hour-12"),
+    $("#hour-1"),
+    $("#hour-2"),
+    $("#hour-3"),
+    $("#hour-4"),
+    $("#hour-5"),
+    $("#hour-6"),
   ];
 
   //This shows the time everytime you refresh the page.
@@ -54,27 +45,23 @@ $(document).ready(function () {
 
   //This function allows the color of the save boxes to change to the current hour.
   function updateTime() {
-    let currentHour = dayjs().format("hh");
-    console.log(currentHour);
-    for (let i = 0; i < scheduleElArray.length; i++) {
-      var blockHour = parseInt(scheduleElArray[i].data("hour"));
-      scheduleElArray[i]
-        .removeClass("future")
-        .removeClass("past")
-        .removeClass("present");
+    let currentHour = new Date().getHours(); // Get the current hour
 
-      if (parseInt(currentHour) > blockHour) {
-        scheduleElArray[i].addClass("past");
-      } else if (parseInt(currentHour) === blockHour) {
-        scheduleElArray[i].removeClass("past");
-        scheduleElArray[i].addClass("present");
+    scheduleElArray.each(function () {
+      let blockHour = parseInt($(this).attr("data-hour")); // Get the block hour
+
+      $(this).removeClass("past present future");
+
+      if (currentHour > blockHour) {
+        $(this).addClass("past");
+      } else if (currentHour === blockHour) {
+        $(this).addClass("present");
       } else {
-        scheduleElArray[i].removeClass("past");
-        scheduleElArray[i].removeClass("present");
-        scheduleElArray[i].addClass("future");
+        $(this).addClass("future");
       }
-    }
+    });
   }
-  //This displays current hour in the console, to make sure its running properly.
+
+  // This displays the current hour and updates the time blocks on page load
   updateTime();
 });
